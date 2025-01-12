bf2dc74cb684429f2de0ea04a5c65ddd253f7b74
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2025-01-12 15:39:22 -0800

    Add: New endpoints for updating and deleting an existing query

--- src/routes/queries.ts
+++ src/routes/queries.ts
@@ -8,6 +8,24 @@ async function queriesRoutes(app: FastifyInstance) {
     component: 'queriesRoutes',
   })
 
+  const handleError = (
+    log: any,
+    message: string,
+    err: any,
+    statusCode: number
+  ) => {
+    log.error({ err }, message)
+    throw new ApiError(message, statusCode)
+  }
+
+  const findQueryById = async (id: string) => {
+    const existingQuery = await prisma.query.findUnique({ where: { id } })
+    if (!existingQuery) {
+      throw new ApiError('Query not found', 404)
+    }
+    return existingQuery
+  }
+
   //Create a new query
   app.post<{
     Body: IQueryBody
@@ -27,8 +45,53 @@ async function queriesRoutes(app: FastifyInstance) {
         })
         reply.status(201).send(newQuery)
       } catch (err: any) {
-        log.error({ err }, 'Error creating new query')
-        throw new ApiError('Failed to create a new query', 500)
+        handleError(log, 'Error creating new query', err, 500)
+      }
+    },
+  })
+
+  //Update an existing query by ID
+  app.put<{
+    Params: { id: string }
+    Body: Partial<IQueryBody> // possible for partial update
+    Reply: { message: string } | IQuery
+  }>('/queries/:id', {
+    async handler(req, reply) {
+      const { id } = req.params
+      const newData = req.body
+
+      log.debug({ id, newData }, 'Updating a query')
+
+      try {
+        await findQueryById(id) //ensure query exists before updating
+        const updatedQuery = await prisma.query.update({
+          where: { id },
+          data: newData,
+        })
+        reply.status(200).send(updatedQuery)
+      } catch (err: any) {
+        handleError(log, 'Error updating query', err, 500)
+      }
+    },
+  })
+
+  //Delete a query by ID
+  app.delete<{
+    Params: { id: string }
+  }>('/queries/:id', {
+    async handler(req, reply) {
+      const { id } = req.params
+
+      log.debug({ id }, 'Deleting a query')
+
+      try {
+        await findQueryById(id) //ensure query exists before deleting
+        const deletedQuery = await prisma.query.delete({
+          where: { id },
+        })
+        reply.status(200).send(deletedQuery)
+      } catch (err: any) {
+        handleError(log, 'Error deleting query', err, 500)
       }
     },
   })
